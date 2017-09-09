45d9241b57a7b890bf536f88bf003a9cfd3fb256
Coolzapper Modding <coolzappergaming@gmail.com>
2017-09-09 13:53:41 +0400

    Delete ModTabs.java

--- src/main/java/com/github/czmodding/evilcraft/tabs/ModTabs.java
+++
@@ -1,9 +0,0 @@
-package com.github.czmodding.evilcraft.tabs;
-
-import net.minecraft.creativetab.CreativeTabs;
-import com.github.czmodding.evilcraft.tabs.SoulsTab;
-public class ModTabs {
-	public static CreativeTabs tabSouls = new SoulsTab(CreativeTabs.getNextID(), "tabSouls");
-	public static CreativeTabs tabBlocks = new SoulsTab(CreativeTabs.getNextID(), "tabBlocks");
-	public static CreativeTabs tabOres = new SoulsTab(CreativeTabs.getNextID(), "tabOres");
-	}
