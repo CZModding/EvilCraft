100ca03f78ad3acfff34fd39d3d4f2a91575d2d9
Coolzapper Modding <coolzappergaming@gmail.com>
2017-09-09 13:55:51 +0400

    Delete ModCrafting.java

--- src/main/java/com/github/czmodding/evilcraft/crafting/ModCrafting.java
+++
@@ -1,6 +0,0 @@
-package com.github.czmodding.evilcraft.crafting;
-
-public class ModCrafting {
-
-	GameRegistry.addRecipe(new ItemStack(ModBlocks.tutorialBlock), new Object[] {"##", "##", '#', ModItems.tutorialItem});
-}
